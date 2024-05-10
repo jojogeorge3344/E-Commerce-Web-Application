c9a39446cbca3f1ea8bff8cfde0c901c4c847a68
JOJO GEORGE <134834693+jojogeorge3344@users.noreply.github.com>
2024-05-10 15:15:38 +0530

    added sound folder and implemented sound effects

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,20 +1,142 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import MaterialUISwitch   from "../../app/layout/Switch"
+import { AppBar, Badge, Box, IconButton, List, ListItem, Snackbar, Toolbar, Typography } from "@mui/material";
+import MaterialUISwitch from "../../app/layout/Switch"
+import { NavLink } from "react-router-dom";
+import { ShoppingCart } from "@mui/icons-material";
+import { useState } from "react";
+import brightNotificationSound from '@sounds/toggle-enable-disable.mp3'; // Dark mode toggle switch sound
+import headerMenuClickSound from '@sounds/header-menu-sound.mp3'; // Header menu clicking sound
+import darkModeEnableSound from '@sounds/Dark-Mode-Enable.mp3'; // Dark mode toggle switch sound
 
-interface Props{
+const midLinks = [
+    { title: 'catalog', path: '/catalog' },
+    { title: 'about', path: '/about' },
+    { title: 'contact', path: '/contact' },
+]
+
+const rightLinks = [
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' }
+]
+
+const navStyle = {
+    color: 'inherit',
+    textDecoration: 'none',
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
+
+interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
-export default function Header({darkMode,handleThemeChange}:Props) {
-    return(
+export default function Header({ darkMode, handleThemeChange }: Props) {
+
+    //Toggle switch popup based on toggle state
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+
+    //Toggle switch sound configure based on toggle state
+    const playSound = (soundFile: string) => {
+        try {
+            const sound = new Audio(soundFile);
+            sound.play();
+        } catch (error) {
+            console.error('Error playing sound:', error);
+        }
+    };
+
+    // Toggle state checking and throwing message and sound
+    const handleDarkModeChange = () => {
+        handleThemeChange();
+        if (darkMode) {
+            setSnackbarMessage('Dark Mode Disabled');
+            playSound(brightNotificationSound); // Use the imported sound file
+        } else {
+            setSnackbarMessage('Dark Mode Enabled');
+            playSound(darkModeEnableSound); // Use the imported sound file
+        }
+    };
+
+    const closeSnackbar = () => {
+        setSnackbarMessage('');
+    };
+
+    return (
+
         // sx={{mb:4} is used for setup the pading btw header box and product images.
-        <AppBar position='static' sx={{mb:3}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    E-Commerce Web Application
-                </Typography>
-                <MaterialUISwitch checked ={darkMode} onChange={handleThemeChange}/>
+        <AppBar position='static' sx={{ mb: 3 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box>
+                    {/* configuring Heading */}
+                    <Typography variant="h6" component={NavLink}
+                        to='/'
+                        sx={navStyle}
+                        onClick={() => playSound(headerMenuClickSound)}
+                    >
+                        E-Commerce Web Application
+                    </Typography>
+
+                    {/* configuring DarkMode button */}
+                    <MaterialUISwitch checked={darkMode} onChange={handleDarkModeChange} />
+                </Box>
+
+                {/* configuring home,abou and contact links */}
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyle}
+                            onClick={() => playSound(headerMenuClickSound)}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box>
+                    {/* configuring cart button */}
+                    <IconButton
+                        size='large'
+                        edge='start'
+                        color='inherit'
+                        sx={{ mr: 2 }}
+                        onClick={() => playSound(headerMenuClickSound)}>
+                        <Badge badgeContent='4' color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    {/* configuring login and register links */}
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyle}
+                                onClick={() => playSound(headerMenuClickSound)}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+
             </Toolbar>
+            <Snackbar
+                open={!!snackbarMessage}
+                autoHideDuration={3000}
+                onClose={closeSnackbar}
+                message={snackbarMessage}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            />
+
         </AppBar>
     )
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    port:3001
+  server: {
+    port: 3001
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+  ],
+  // Add a file loader to handle non-JavaScript file imports
+  resolve: {
+    alias: {
+      // Mentioning the MP3 files containing folder name
+      '@sounds': '/public/Sounds'
+    }
+  },
+  // Configure the file loader to handle MP3 files. the all sounds should mention here
+  optimizeDeps: {
+    include: [
+      '@sounds/toggle-enable-disable.mp3',
+      '@sounds/header-menu-sound.mp3',
+      '@sounds/Dark-Mode-Enable.mp3'
+
+    ]
+  }
+});
