1927d6ec3352d240e0ea754531be3d2d495bf30f
JOJO GEORGE <134834693+jojogeorge3344@users.noreply.github.com>
2024-05-11 12:27:56 +0530

    individual product view
    implemented individual product view design.

--- client/src/Features/Catalog/ProductDetails.tsx
+++ client/src/Features/Catalog/ProductDetails.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/Models/product";
+import { Style } from "@mui/icons-material";
 
 export default function ProductDetailsPage(){
 const{id}=useParams<{id:string}>();
@@ -20,8 +21,41 @@ if(loading)return <h3>Loading...</h3>
 
 if(!product) return<h3>Product Not Found</h3>
     return(
-        <Typography variant="h2">
-            {product.name}
-        </Typography>
+       <Grid container spacing={6}>
+        <Grid item xs={6}>
+            <img src={product.pictureUrl} alt={product.name} style={{width:'100%'}}/>
+            </Grid>
+            <Grid item xs={6}>
+            <Typography variant='h3'>{product.name}</Typography>
+            <Divider sx={{mb:2}}/>
+            <Typography variant='h4' color='secondary'>${(product.price/100).toFixed(2)}</Typography>
+            <TableContainer>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>{product.name}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Description</TableCell>
+                            <TableCell>{product.description}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Type</TableCell>
+                            <TableCell>{product.type}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Brand</TableCell>
+                            <TableCell>{product.brand}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Quantity in Stock</TableCell>
+                            <TableCell>{product.quantityInStock}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </Grid>
+        </Grid>
     )
 }
